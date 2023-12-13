00547703017d991210ca947581ed9c9e5d55c9d9
zzho-o <78499273+zzho-o@users.noreply.github.com>
2023-12-13 21:54:36 +0900

    feat:appWrapper

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ReactNode } from "react";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+
+const AppWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <RecoilRoot>
+      <MiddleApp>{children}</MiddleApp>
+    </RecoilRoot>
+  );
+};
+
+const MiddleApp = ({ children }: { children: ReactNode }) => {
+  return <>{children}</>;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
